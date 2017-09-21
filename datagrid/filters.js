2b3ae3bb9d0b704bad8c9e1ffd4fedb616e3a79f
Adam Jun <adam@bluesky.pro>
2017-09-21 21:56:12 +0200

    filter component render update

--- datagrid/filters.js
+++ datagrid/filters.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { observer } from 'mobx-react'
-import { DropdownButton, MenuItem, Button, InputGroup } from 'react-bootstrap'
+import {observer} from 'mobx-react'
+import {DropdownButton, MenuItem, Button, InputGroup, Tooltip, OverlayTrigger} from 'react-bootstrap'
 import FilterBases from 'react-mobx-admin/components/common/datagrid/filters'
 
 // dropdown with available filters
@@ -39,12 +39,25 @@ class Controls extends FilterBases.ControlsBase {
 
   renderControl(filter, name, state, onHide, onUpdateValue) {
 
+    const toolTip = (
+      <Tooltip id="tooltip"><strong>{filter.info}</strong></Tooltip>
+    )
+
     return (
       <div className={`form-field form-group filter-${name}`} style={styles.chip} key={name}>
+        <OverlayTrigger placement="right" overlay={toolTip}>
+          <strong>{filter.title || name}</strong>     
+        </OverlayTrigger>    
         <InputGroup>
           <Button onClick={onHide} style={{float: 'left'}}>x</Button>
           <div style={{float: 'right'}}>
-            <filter.component record={state.cv.filters} attr={name} onChange={onUpdateValue} />
+            <filter.component record={state.cv.filters} attr={name} onChange={onUpdateValue} 
+              onKeyPress={(e) => {
+                if (e.charCode === 13) {
+                  e.preventDefault()
+                  state.applyFilters()
+                }
+              }} />
           </div>
         </InputGroup>
       </div>
