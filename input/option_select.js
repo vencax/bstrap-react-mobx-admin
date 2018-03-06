c42be89dd9433cbbf15cb65a9a12cfe5907d4ec7
vencax <vencax@centrum.cz>
2018-03-06 08:27:45 +0100

    fix

--- input/option_select.js
+++ input/option_select.js
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types'
 import {observer} from 'mobx-react'
 import {Radio, FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap'
 
-const OptionSelect = ({attr, record, onChange, errors, label, disabled}) => {
+const OptionSelect = ({
+  attr, record, onChange, errors, label, disabled, options
+}) => {
   const val = record.get(attr)
   const errorText = errors.has(attr) ? errors.get(attr) : undefined
   const validationState = errorText ? 'error' : null
@@ -12,14 +14,17 @@ const OptionSelect = ({attr, record, onChange, errors, label, disabled}) => {
     <FormGroup validationState={validationState}>
       <ControlLabel>{label}</ControlLabel>
       <div>
-        <Radio name={attr} checked={val === 1} disabled={disabled} inline
-          onClick={() => onChange(attr, 1)}>
-          ANO
-        </Radio>{' '}
-        <Radio name={attr} checked={val === 0} disabled={disabled} inline
-          onClick={() => onChange(attr, 0)}>
-          NE
-        </Radio>
+        {
+          options.map((i, idx) => {
+            return (
+              <Radio key={idx} name={attr} checked={val === i.value}
+                disabled={disabled} inline
+                onClick={() => onChange(attr, i.value)}>
+                {i.label}
+              </Radio>
+            )
+          })
+        }
       </div>
       {errorText ? <HelpBlock>{errorText}</HelpBlock> : null}
     </FormGroup>
